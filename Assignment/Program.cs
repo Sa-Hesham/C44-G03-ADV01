0b240bce1ebb5e51ca7b21e0d3a17cecce10aae7
Sa-Hesham <elsayedheshamdarwish@gmail.com>
2025-08-16 20:35:27 +0300

    Edit

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -6,23 +6,41 @@ namespace Assignment
     {
         static void Main(string[] args)
         {
+            #region Q1
+
+            //var intRange = new Range<int>(10, 20);
+
+            //Console.WriteLine(intRange.ISRange(15)); 
+            //Console.WriteLine(intRange.ISRange(25)); 
+            //Console.WriteLine(intRange.GetLength());      
+
+            //var doubleRange = new Range<double>(5.5, 9.5);
+            //Console.WriteLine(doubleRange.ISRange(7.2)); // True
+            //Console.WriteLine(doubleRange.GetLength());
+
+            #endregion
+
+
+            #region Q2
+
             //ArrayList list = new ArrayList() { 5, 4, 3, 2, 1 };
             //Helper.ReverseArray(list);
             //foreach (int i in list) { 
-            
+
             //    Console.WriteLine(i);
-            
-             
-            //}
+
+
+            //} 
+            #endregion
 
             #region Q3
-            List<int> Numbers = new List<int>() { 1,2,3,4,5,6,7,8,9,10};
+            //List<int> Numbers = new List<int>() { 1,2,3,4,5,6,7,8,9,10};
 
-            List<int> Evens = Helper.GetEvennumbers(Numbers);
-            foreach (int item in Evens)
-            {
-                Console.WriteLine(item);
-            }
+            //List<int> Evens = Helper.GetEvennumbers(Numbers);
+            //foreach (int item in Evens)
+            //{
+            //    Console.WriteLine(item);
+            //}
             #endregion
         }
     }
