e8401d5a18cc0c4e0a09209f8032b11b1e0a6618
Sa-Hesham <elsayedheshamdarwish@gmail.com>
2025-08-16 19:57:06 +0300

    Question-1

---
+++ Assignment/Range.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    internal class Range <T> where T: IComparable<T>,INumber<T>
+    {
+   
+
+        public T Maximun { get; set; }
+        public T Minimum { get; set; }
+
+
+        public Range(T maximum  , T minimum)
+        {
+            if (minimum.CompareTo(minimum) > 0)
+            {
+                Console.WriteLine("min is greater than max");
+            }
+
+            Maximun = maximum;
+            Minimum = minimum;
+
+        }
+
+
+        public bool ISRange(T value)
+        {
+           return   (value.CompareTo(Minimum) >= 0 && value.CompareTo(Maximun) <= 0) ;
+                
+        }
+
+        public T GetLength()
+        {
+            return Maximun - Minimum;
+        }
+    }
+}
